31d8d15c43cdc014075d9a881572ad306aaa5219
Fauzi <fauzi_farhat@hotmail.com>
2018-08-24 15:24:02 -0300

    Adicionado exercício 2

---
+++ Aula10/src/main/java/br/com/db1/exercicio02/Roupa.java
@@ -0,0 +1,25 @@
+package br.com.db1.exercicio02;
+
+import br.com.db1.exercicio02.type.Tamanho;
+
+public class Roupa {
+	
+	Tamanho tamanho;
+	private String marca;
+	private Integer anoFabricacao;
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public Integer getAnoFabricacao() {
+		return anoFabricacao;
+	}
+	public void setAnoFabricacao(Integer anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
+	}
+	
+	
+}
---
+++ Aula10/src/main/java/br/com/db1/exercicio02/type/Tamanho.java
@@ -0,0 +1,11 @@
+package br.com.db1.exercicio02.type;
+
+public enum Tamanho {
+	
+	P,
+	M,
+	G;
+	
+	
+	
+}
