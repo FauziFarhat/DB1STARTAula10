7165272db87faad14c28200a94ca82b3cc6a22be
Fauzi <fauzi_farhat@hotmail.com>
2018-08-24 16:12:10 -0300

    Adicionado exercício 5

---
+++ Aula10/src/main/java/br/com/db1/exercicio05/TransporteAquatico.java
@@ -0,0 +1,18 @@
+package br.com.db1.exercicio05;
+
+public class TransporteAquatico {
+	private JetSki jetski;
+	private Canoa canoa;
+	private Barco barco;
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
